d5e0a6dbeebe78e504682d7e699192128af447bb
Souvik Sen <theerosanninjiraya@gmail.com>
2025-05-04 11:43:37 +0600

    Create A+B_Problem.cpp

---
+++ A+B_Problem.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int32_t main(){
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    cout<<start;
+    return 0;
+}
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a,b;
+//     cin>>a>>b;
+//     cout<<a+b;
+// }
