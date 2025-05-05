dbf8e836271891d2632a6c5c15b364226e445069
Souvik Sen <theerosanninjiraya@gmail.com>
2025-05-05 21:33:16 +0600

    Create Above_the_Secundary_Diagonal.cpp

---
+++ Above_the_Secundary_Diagonal.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int i,j;
+    char ch;
+    double M[12][12],sum=0;
+    cin>>ch;
+    for(i=0;i<12;i++){
+        for(j=0;j<12;j++){
+            cin>>M[i][j];
+            if(j<(11-i)){
+                sum+=M[i][j];
+            }
+        }
+    }
+    if(ch=='S')
+    cout<<fixed<<setprecision(1)<<sum<<endl;
+    if(ch=='M')
+    cout<<fixed<<setprecision(1)<<sum/66.0<<endl;
+}
